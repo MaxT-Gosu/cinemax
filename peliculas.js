574929d72a18d6aaadd1ec43d2a0a865e40632a3
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-04 21:01:06 -0500

    Add files via upload

--- peliculas.js
+++ peliculas.js
@@ -6,16 +6,24 @@ const baseDeDatos = [
         poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/cZJaa2ZOp3wiXXw0mqqtp3d326j.jpg", 
         bg: "https://image.tmdb.org/t/p/original/uBfAZuAsEmIQ66N2OTZ8ytLEV6.jpg", 
         video: "https://www.burstcloud.co/embed/b747167e31d4ccd34d5c5fadd32d1c9f737ef12869685343a96c9ccee9974492/her%20ella.mp4", 
-        descripcion: "Dos agentes altamente entrenados se unen a la distancia tras ser enviados a proteger bordes opuestos de un abismo misterioso. Cuando la maldad emerge de él, tienen que trabajar juntos para sobrevivir a lo que yace dentro del abismo.", 
+        descripcion: "Dos agentes altamente entrenados se unen a la distancia...", 
         fecha: "2025", 
-        tags: "Romance, Ciencia ficción y Suspense",
-        etiqueta: "EXCLUSIVA", // La cintilla roja arriba
-        coincidencia: "98% para ti", // <-- Lo que querías recuperar
-        calidad: "HD" // <-- El cuadrito de calidad
+        tags: "Romance, Ciencia ficción",
+        etiqueta: "EXCLUSIVA",
+        coincidencia: "98% para ti",
+        calidad: "HD"
     },
-    /* Para agregar otra película:
-    1. Copia desde la llave { hasta la llave }
-    2. Pon una coma después de la última llave
-    3. Pega y cambia los datos
-    */
+    { 
+        id: "P2", 
+        titulo: "Compañera perfecta", 
+        poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/m68uoULpxeDkmCetbhftXCgP1TZ.jpg", 
+        bg: "https://image.tmdb.org/t/p/original/9p2tVEEWw1M92Pt63Vbq7EjBpvk.jpg", 
+        video: "https://www.burstcloud.co/embed/1b72b0a9273e43f0f1f304d59d91943232442da6504940459eed6c0c5b0bc553/Companera%20perfecta.mp4", 
+        descripcion: "La muerte de un multimillonario desencadena una cadena de acontecimientos para Iris y sus amigos durante un viaje de fin de semana a su propiedad junto al lago.", 
+        fecha: "2025", 
+        tags: "Terror, Ciencia ficción y Suspense",
+        etiqueta: "ESTRENO",
+        coincidencia: "80% para ti",
+        calidad: "HD"
+    }
 ];
