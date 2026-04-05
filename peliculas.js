849ec08fbfb07ad969a9da4cd004ad3375f6755a
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-04 22:15:51 -0500

    Add files via upload

--- peliculas.js
+++ peliculas.js
@@ -25,5 +25,31 @@ const baseDeDatos = [
         etiqueta: "ESTRENO",
         coincidencia: "80% para ti",
         calidad: "HD"
-    }
+    },
+	{ 
+        id: "P3", 
+        titulo: "Napoleón", 
+        poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/ytFOXyghxLzAM4KZyazDdEkM66q.jpg", 
+        bg: "https://image.tmdb.org/t/p/original/33pMXav77ICRnceEBLhL8lXTywv.jpg", 
+        video: "https://www.burstcloud.co/embed/ff55dabfd9262fee808e2f57f99ab58a2b7c83463444f88f9046df9ee5cf17b0/NAPOLEON.mp4", 
+        descripcion: "Una mirada personal a los orígenes del líder militar francés y su rápido y despiadado ascenso a emperador.", 
+        fecha: "2023", 
+        tags: "Historia, Bélica y Romance",
+        etiqueta: "ESTRENO",
+        coincidencia: "63% para ti",
+        calidad: "HD"
+    },
+	{ 
+        id: "P4", 
+        titulo: "Corazón Valiente", 
+        poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/6snCuOuKwWnATAOTTumAtRImuoo.jpg", 
+        bg: "https://image.tmdb.org/t/p/original/8lBcqakI3F19NWkXdqE0M8W76b9.jpg", 
+        video: "https://www.burstcloud.co/embed/5f9605a235e4a811d417790a769810725f0ce7dac06bdd27251fa0470f7dcacb/corazonvaliente.mp4", 
+        descripcion: "Drama histórico que narra la vida de William Wallace, un héroe escocés del siglo XIII que lidera una rebelión contra el dominio inglés", 
+        fecha: "1995 ", 
+        tags: "Acción, Drama, Historia y Bélica",
+        etiqueta: "ESTRENO",
+        coincidencia: "79% para ti",
+        calidad: "HD"
+    },
 ];
