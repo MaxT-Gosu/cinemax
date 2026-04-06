7e81c3528842ca775fe4b7a5f9af1b19f6b7c0e1
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-06 08:14:38 -0500

    Add files via upload

--- series.js
+++ series.js
@@ -1,29 +1,58 @@
 const baseDeSeries = [
     { 
-        id: "S-01", 
-        titulo: "Imperio de Mentiras", 
-        poster: "https://via.placeholder.com/300x450/1a1a1a/ffffff?text=Imperio+Mentiras", 
-        bg: "https://via.placeholder.com/1280x720/1a1a1a/ffffff?text=Banner+Imperio", 
-        estudio: "HBO Max", 
-        fecha: "2024", 
-        actriz: "Pedro Pascal, Bella Ramsey", 
-        tags: "Drama, Thriller",
-        etiqueta: "NUEVA TEMPORADA",
-        // NUEVA ESTRUCTURA DE TEMPORADAS Y EPISODIOS
+        id: "S1", 
+        titulo: "Alien: Earth", 
+        poster: "https://www.themoviedb.org/t/p/w600_and_h900_face/8IIPZe4V2xNYFWY7lBY6GpN2L8O.jpg", 
+        bg: "https://image.tmdb.org/t/p/original/5UDd3JBS8qkCG9QBGVTrqBtibGj.jpg", 
+        descripcion: "Una nave espacial se estrella, y una joven junto a soldados enfrenta una amenaza planetaria.", 
+        fecha: "2025", 
+        tags: "Sci-Fi & Fantasy y Drama",
+        etiqueta: "ESTRENO",
+        coincidencia: "95% para ti",
+        calidad: "HD",
         temporadas: [
             {
                 numero: 1,
                 episodios: [
-                    { ep: 1, titulo: "El Comienzo", video: "https://www.w3schools.com/html/mov_bbb.mp4" },
-                    { ep: 2, titulo: "La Traición", video: "https://www.w3schools.com/html/mov_bbb.mp4" }
+                    { ep: 1, titulo: "El País de Nunca Jamás", video: "https://www.burstcloud.co/embed/2e24babedc21bccf1705c1323410df3fac1149ccf94a89d69a2560040d07b5ee/T1E1.mp4" },
+                    { ep: 2, titulo: "Mr. October", video: "https://www.burstcloud.co/embed/60e17f19ebf3890feee41d4cf5cf6e334966429098cf61c34e3b8f80fd23fa75/T1E2.mp4" },
+					{ ep: 3, titulo: "Metamorfosis", video: "https://www.burstcloud.co/embed/8abf06cd2474c752cd0429c53f142a2e37c1a7e3645c0ec153c057d06861cdc7/T1E3.mp4" },
+                    { ep: 4, titulo: "Observación", video: "https://www.burstcloud.co/embed/29930495cf0ce11c9596fecdefe1ba63f4b8f0810cfdc4e02afba5d59e14e411/T1E4.mp4" },
+					{ ep: 5, titulo: "En el espacio, nadie...", video: "ttps://www.burstcloud.co/embed/6418f4637a2632eb768f8be39b1c232983fc0191f3f7de5f0d1c9461a9004a13/T1E5.mp4" },
+                    { ep: 6, titulo: "El vuelo", video: "https://www.burstcloud.co/embed/4b2cd7378732d5a275ed5a4540db5e3083d1b66b41783f362155eb00ca919a0c/T1E6.mp4" },
+					{ ep: 7, titulo: "Emergencia", video: "https://www.burstcloud.co/embed/ccd282bf105bce64ed573101396d71f77cef9a598ffea13ddfe4afa1034ea770/T1E7.mp4" },
+                    { ep: 8, titulo: "Los verdaderos monstruos", video: "https://www.burstcloud.co/embed/849d121a0ad4f430f240729452c2e2b19e24e2b3ea1b150dc682db9dbedc57d5/T1E8.mp4" },
+                ]
+            }
+        ]
+    },
+	{ 
+        id: "S-AQUÍ_EL_ID", 
+        titulo: "NOMBRE DE LA SERIE", 
+        poster: "URL_DEL_POSTER", 
+        bg: "URL_DEL_BANNER_HORIZONTAL", 
+        descripcion: "Escribe aquí la sinopsis de la serie...", 
+        fecha: "2026", 
+        tags: "Género 1, Género 2",
+        etiqueta: "ESTRENO / NUEVA TEMPORADA",
+        coincidencia: "99% para ti",
+        calidad: "HD",
+        temporadas: [
+            {
+                numero: 1,
+                episodios: [
+                    { ep: 1, titulo: "Título del Cap 1", video: "URL_DEL_VIDEO" },
+                    { ep: 2, titulo: "Título del Cap 2", video: "URL_DEL_VIDEO" },
+                    { ep: 3, titulo: "Título del Cap 3", video: "URL_DEL_VIDEO" }
                 ]
             },
             {
                 numero: 2,
                 episodios: [
-                    { ep: 1, titulo: "El Regreso", video: "https://www.w3schools.com/html/mov_bbb.mp4" }
+                    { ep: 1, titulo: "Título del Cap 1", video: "URL_DEL_VIDEO" },
+                    { ep: 2, titulo: "Título del Cap 2", video: "URL_DEL_VIDEO" }
                 ]
             }
         ]
-    }
+    },
 ];
