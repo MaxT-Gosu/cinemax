715a1b707283277918fbb6eb6bc39b7ea973ff7f
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-04-06 08:26:46 -0500

    Add files via upload

--- series.js
+++ series.js
@@ -26,31 +26,55 @@ const baseDeSeries = [
             }
         ]
     },
-	{ 
-        id: "S-AQUÍ_EL_ID", 
-        titulo: "NOMBRE DE LA SERIE", 
-        poster: "URL_DEL_POSTER", 
-        bg: "URL_DEL_BANNER_HORIZONTAL", 
-        descripcion: "Escribe aquí la sinopsis de la serie...", 
-        fecha: "2026", 
-        tags: "Género 1, Género 2",
-        etiqueta: "ESTRENO / NUEVA TEMPORADA",
-        coincidencia: "99% para ti",
+{ 
+        id: "S2", 
+        titulo: "True Detective", 
+        poster: "https://image.tmdb.org/t/p/w600_and_h900_face/e0JrT2GTqeVv26oi1zxDHf0d9t.jpg", 
+        bg: "https://image.tmdb.org/t/p/original/e0JrT2GTqeVv26oi1zxDHf0d9t.jpg", 
+        descripcion: "Serie de antología de HBO que sigue investigaciones criminales intensas que llevan a sus detectives al borde de la obsesión y la locura.", 
+        fecha: "2014", 
+        tags: "Acción • Suspense • Drama",
+        etiqueta: "PREMIUM",
+        coincidencia: "8.3/10",
         calidad: "HD",
         temporadas: [
             {
                 numero: 1,
                 episodios: [
-                    { ep: 1, titulo: "Título del Cap 1", video: "URL_DEL_VIDEO" },
-                    { ep: 2, titulo: "Título del Cap 2", video: "URL_DEL_VIDEO" },
-                    { ep: 3, titulo: "Título del Cap 3", video: "URL_DEL_VIDEO" }
+                    { ep: 1, titulo: "The Long Bright Dark", video: "https://www.burstcloud.co/embed/b0fbeed5d4672e10cd403319fffb0d16ffeed7c7f461b2164060e3e319aafa4d/1X01.mp4" },
+                    { ep: 2, titulo: "Seeing Things", video: "https://www.burstcloud.co/embed/edae11e6a482ee05342e91147c669fe4c987c9e534c7d65f61a8489b6073b187/1X02.mp4" },
+                    { ep: 3, titulo: "The Locked Room", video: "https://www.burstcloud.co/embed/c8649561493a64640afcb1b83cbeafb688b635971b7340524777d3ec2000d095/1X03.mp4" },
+                    { ep: 4, titulo: "Who Goes There", video: "https://www.burstcloud.co/embed/77c002a3b80fee7604f8ccd8520c33555bbef36beb3ce8aa9212799f419e5cb2/1X04.mp4" },
+                    { ep: 5, titulo: "The Secret Fate of All Life", video: "https://www.burstcloud.co/embed/83420c356c9038a4ae40f1c037d21d7d9cf809c1c109b57b2987defde45f4510/1X05.mp4" },
+                    { ep: 6, titulo: "Haunted Houses", video: "https://www.burstcloud.co/embed/e4e920c6977b964e13d8e0aba5c797866df2939d276a7b3e50c64de9a0a4fc13/1X06.mp4" },
+                    { ep: 7, titulo: "After You've Gone", video: "https://www.burstcloud.co/embed/c291e7f1eacac0a2426d83c86dea833d24aaf0bb42e4b0290e55299b44abf6bc/1X07.mp4" },
+                    { ep: 8, titulo: "Form and Void", video: "https://www.burstcloud.co/embed/af0e32dc5947a41b6038d687c4da9b2b9de1838cf2bb970fee30aa2b0805dde9/1X08.mp4" }
                 ]
             },
             {
                 numero: 2,
                 episodios: [
-                    { ep: 1, titulo: "Título del Cap 1", video: "URL_DEL_VIDEO" },
-                    { ep: 2, titulo: "Título del Cap 2", video: "URL_DEL_VIDEO" }
+                    { ep: 1, titulo: "The Western Book Of The Dead", video: "https://www.burstcloud.co/embed/5e3689f038cec561519ac2a881998f117e58233d34b92cbefcb808d130b2fce6/2X01.mp4" },
+                    { ep: 2, titulo: "Night Finds You", video: "https://www.burstcloud.co/embed/004a4b99f6edc753b3916b477a8175fc842e92df06f2c664214f7896dbd0cf77/2X02.mp4" },
+                    { ep: 3, titulo: "Maybe Tomorrow", video: "https://www.burstcloud.co/embed/13024bfef024d413323da94d9f301c45f76fee73a34d968204286f31c5c7674d/2X03.mp4" },
+                    { ep: 4, titulo: "Down Will Come", video: "https://www.burstcloud.co/embed/f6b4def0650b77d1f8f8adac7f59ca6e467ff16aef62a7c28d0d686a0ef5aae5/2X04.mp4" },
+                    { ep: 5, titulo: "Other Lives", video: "https://www.burstcloud.co/embed/414b810acd556aafe6278e0eae07c85b799514817d61f8da1776198fd1246f65/3x5.mp4" },
+                    { ep: 6, titulo: "Church In Ruins", video: "https://www.burstcloud.co/embed/a2b03380ad42dfce6fafeb0a8eee5fe336538d34a256500a1075da24031372ee/3x6.mp4" },
+                    { ep: 7, titulo: "Black Maps And Motel Rooms", video: "https://www.burstcloud.co/embed/19e5a448455ee8fb0882f5912e54cc4f15522d4e495127e1fd5162de64c5674a/3x7.mp4" },
+                    { ep: 8, titulo: "Omega Station", video: "https://www.burstcloud.co/embed/414b810acd556aafe6278e0eae07c85b799514817d61f8da1776198fd1246f65/3x8.mp4" }
+                ]
+            },
+            {
+                numero: 3,
+                episodios: [
+                    { ep: 1, titulo: "The Great War and Modern Memory", video: "https://www.burstcloud.co/embed/fafc191a6d5c4491f185b91622b2984961af3a6341b7795b57c5d2d6a50ddd71/3X01.mp4" },
+                    { ep: 2, titulo: "Kiss Tomorrow Goodbye", video: "https://www.burstcloud.co/embed/da247152149e75adbb2ff8223cb5f16568dbb8042cb826d58762bbbe536de096/3X02.mp4" },
+                    { ep: 3, titulo: "The Big Never", video: "https://www.burstcloud.co/embed/2d4718db5eee7f44d582dcfecbb441df977bc9ec9e36778d9ee344bea1e9b731/3X03.mp4" },
+                    { ep: 4, titulo: "The Hour and the Day", video: "https://www.burstcloud.co/embed/3368e841acaeb3304e04038573e82035a4bc23f7c2c57ad1b3809c9e5e8713ee/3X04.mp4" },
+                    { ep: 5, titulo: "If You Have Ghosts", video: "https://www.burstcloud.co/embed/6a935b4fbf2ae986c04bbcc40ac90470a8ee681acdae7100e924821340c22dc5/3X05.mp4" },
+                    { ep: 6, titulo: "Hunters in the Dark", video: "https://www.burstcloud.co/embed/d473b1877c008332f6baa6709197ef27de6a9968ccfa6f5601c58c07ed9df1d5/3X06.mp4" },
+                    { ep: 7, titulo: "The Final Country", video: "https://www.burstcloud.co/embed/785ee44f71da2ff3fb03ff23ab2989bbd8311733be1955521cf3dab9461c00ff/3X07.mp4" },
+                    { ep: 8, titulo: "Now Am Found", video: "https://www.burstcloud.co/embed/bba06f6e2395c1f8b0dbe8fd8f56195d9a0cc6939b8e899ee397e219b6d68447/3X08.mp4" }
                 ]
             }
         ]
