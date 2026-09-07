142f287a3424a0bd2dd51b1ce96b822ed24dbc30
MaxT-Gosu <69736443+MaxT-Gosu@users.noreply.github.com>
2026-09-07 16:59:59 -0500

    Add files via upload

--- series.js
+++ series.js
@@ -691,7 +691,8 @@ const baseDeSeries = [
                 episodios: [
                     { ep: 1, titulo: "Episodio 1", video: "https://appcine.b-cdn.net/Cinemax/Series/Linternas/Lanterns%20S01e01%20Amzn%20Web-Dl%20Dual%20Audio%20Hd%20%5BBoosterspa%5D.mp4" },
 					{ ep: 2, titulo: "Episodio 2", video: "https://appcine.b-cdn.net/Cinemax/Series/Linternas/Lanterns%20S01e02%20Amzn%20Web-Dl%20Dual%20Audio%20Hd%20%5BBoosterspa%5D.mp4" },
-					{ ep: 3, titulo: "Episodio 3", video: "https://appcine.b-cdn.net/Cinemax/Series/Linternas/Lanterns%20S01e03%20Amzn%20Web-Dl%20Dual%20Audio%20Hd%20%5BBoosterspa%5D.mp4" }
+					{ ep: 3, titulo: "Episodio 3", video: "https://appcine.b-cdn.net/Cinemax/Series/Linternas/Lanterns%20S01e03%20Amzn%20Web-Dl%20Dual%20Audio%20Hd%20%5BBoosterspa%5D.mp4" },
+					{ ep: 4, titulo: "Episodio 4", video: "https://appcine.b-cdn.net/Cinemax/Series/Linternas/Lanterns%20S01e04%20Amzn%20Web-Dl%20Dual%20Audio%20Hd%20%5BBoosterspa%5D.mp4" }
                 ]
             }
         ]
@@ -725,7 +726,7 @@ const baseDeSeries = [
 			{
                 numero: 2,
                 episodios: [
-                    { ep: 1, titulo: "Episodio 1", video: "https://appcine.b-cdn.net/Cinemax/Series/Linternas/Lanterns%20S01e01%20Amzn%20Web-Dl%20Dual%20Audio%20Hd%20%5BBoosterspa%5D.mp4" },
+                    { ep: 1, titulo: "Episodio 1", video: "https://appcine.b-cdn.net/Cinemax/Series/Materia%20oscura/temp2/Mtriaoscrs02e01.mp4" },
 					
                 ]
             }
